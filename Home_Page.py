a02dfdfa17139bf85a93db107fdf59ed62b1ba8b
rqkun <116709458+rqkun@users.noreply.github.com>
2023-05-19 20:00:00 +0700

    Update Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -14,19 +14,15 @@ tools.add_logo()
 
 st.markdown(
     """
-    ### About - English 
+    ## About
     This is a project for DIPR430685_22_2_10 🎥 class.
     The project was built on streamlit with 3 different pages.
-    Which include:
+    ### Which include:
     - 👤 Face Detection  
     - 👥 Face Regconition
     - 📷 Image Processing (Chapter 3,4,5,9)
-    ### About - Vietnamese
-    Đây là project cuối kì của môn Xử lý ảnh số (DIPR430685_22_2_10 🎥)
-    Project được dựng trên framework streamlit với 3 trang khác nhau
-    tương ứng với 3 mục:
-    - 👤 Face Detection: Phát hiện khuôn mặt
-    - 👥 Face Regconition: Nhận dạng khuôn mặt
-    - 📷 Image Processing: Xử lý ảnh (Chương 3,4,5,9)
+    ### Contact:
+    - Gmail:
+    - Github:
 """
 )
